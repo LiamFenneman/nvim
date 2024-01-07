3d0b0d6abf0f7b4db8b19d863f4b26fe4c665069
LiamFenneman <liamfennemanbusiness@gmail.com>
2024-01-07 14:51:12 +1300

    Disable reload notification

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -11,4 +11,10 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("plugins")
+local opts = {
+  change_detection = {
+    notify = false,
+  },
+}
+
+require("lazy").setup("plugins", opts)
