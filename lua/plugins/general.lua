1451e7cd9d375ba2c0b48d6d64e66446a2de517a
LiamFenneman <liamfennemanbusiness@gmail.com>
2024-01-07 16:42:26 +1300

    Add todo-comments

--- lua/plugins/general.lua
+++ lua/plugins/general.lua
@@ -23,4 +23,23 @@ return {
     "numToStr/Comment.nvim",
     config = true,
   },
+
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("todo-comments").setup({})
+
+      vim.keymap.set("n", "]t", function()
+        require("todo-comments").jump_next()
+      end, { desc = "Next TODO comment" })
+
+      vim.keymap.set("n", "[t", function()
+        require("todo-comments").jump_prev()
+      end, { desc = "Previous TODO comment" })
+
+      vim.keymap.set("n", "<leader>st", ":TodoTelescope<CR>", { desc = "[S]earch [T]ODO comments" })
+      vim.keymap.set("n", "<leader>td", ":TodoTrouble<CR>", { desc = "Open [T]ODO [T]rouble" })
+    end
+  },
 }
